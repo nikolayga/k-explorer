d92c8ee78c9316b15abaf10dd83640542344f027
nikolayga <71999088+nikolayga@users.noreply.github.com>
2020-09-28 23:43:32 +0600

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 var config = {};
 
-config.endpoint_cron = "...";
-config.endpoint_web = "...";
+config.endpoint_cron = "https://mainnet.infura.io/v3/...";
+config.endpoint_web = "https://mainnet.infura.io/v3/...";
 config.electrumx_server = "...";
 config.electrumx_port = 8443;
 config.electrumx_protocol = "wss";
